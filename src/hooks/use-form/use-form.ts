cf798691d7918b0a60ea504349e44ca504fb96f5
Dr-NULL <silvaaguilarfelipe@gmail.com>
2021-11-04 15:05:38 -0300

    upload a hook

--- src/hooks/use-form/use-form.ts
+++ src/hooks/use-form/use-form.ts
@@ -1,6 +1,53 @@
 import { ChangeEvent, useState } from 'react';
 
-export function useForm<T = Record<string, string>>(initialState: Record<keyof T, string>) {
+/**
+ * ### Description:
+ * Creates a hook to handle form changes, and getting easyfully the values of every
+ * input.
+ * 
+ * ### How to use:
+ * @example
+ * ```tsx
+ * export const Component = () => {
+ *     // The hook instantiation...
+ *     const { data, reset, handleChange } = useForm({ message: null });
+ * 
+ *     // The template
+ *     return (
+ *         <form>
+ *             <label
+ *             htmlFor="message">
+ *                 Message here:
+ *             </label>
+ *   
+ *             <input
+ *             type="text"
+ *             id="message"
+ *             value={data.message}
+ *             onChange={handleChange} />
+ *         </form>
+ *     );
+ * };
+ * ```
+ * 
+ * @param initialState Inital state of the form, use the id or the name of the
+ * input has key, and a string has value.
+ * @returns a Hook to handle form state. this hook is an object with 3 keys:
+ * @example
+ * ```ts
+ * interface FormHook {
+ *     // This is an object with the current form values.
+ *     data: T;
+ * 
+ *     // This is a function to reset the form.
+ *     reset(): void;
+ * 
+ *     // This is an event to you musto to attach in all inputs.
+ *     handleChange(e: FormEvent): void;
+ * }
+ * ```
+ */
+export function useForm<T extends Record<string, any>>(initialState: Record<keyof T, any>) {
     const [data, setForm] = useState(initialState);
 
     const handleChange = (e: ChangeEvent) => {
